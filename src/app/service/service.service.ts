d8c9fc1c7f13c83354d161805cadd2ff5d84212e
German Ramirez Gaviria <gramirezg10@gmail.com>
2020-10-16 09:03:07 -0500

    se crea la variable de la url del servicio en el environment y en el service

--- src/app/service/service.service.ts
+++ src/app/service/service.service.ts
@@ -1,10 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
+
+  public url:string = environment.urlApi
 
   constructor(
     public _http: HttpClient
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  urlApi: 'https://h3nrpdngg7.execute-api.us-east-1.amazonaws.com/prod/colaboradores'
 };
 
 /*
